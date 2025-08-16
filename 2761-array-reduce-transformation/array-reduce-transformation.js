0859334cabff410b39303bf9d5db1c41fc1dd753
Akash-develop <akash.p02.dev@gmail.com>
2025-08-16 18:52:56 +0530

    Time: 33 ms (95.78%) | Memory: 54.1 MB (72.51%) - LeetSync

---
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+        let val = init; 
+
+    for (let i = 0; i < nums.length; i++) {
+        val = fn(val, nums[i]);
+    }
+
+    return val;
+};
+
+let nums = [1, 2, 3, 4];
+let fn = function sum(accum, curr) { return accum + curr; };
+let init = 0;
+
+console.log(reduce(nums, fn, init));
